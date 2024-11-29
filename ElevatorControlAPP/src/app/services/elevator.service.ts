bebcd41b50e0d66437558250f9b313e0c1ee34f2
Ronal Gelvez <rmgelvez@hotmail.com>
2024-11-29 16:46:04 -0500

    Elevator Front Angular

---
+++ ElevatorControlAPP/src/app/services/elevator.service.ts
@@ -0,0 +1,47 @@
+// import { Injectable } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class ElevatorService {
+
+//   constructor() { }
+// }
+
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ElevatorService {
+  private apiUrl = 'https://localhost:5001/api/elevator'; // URL del backend
+
+  constructor(private http: HttpClient) {}
+
+  getState(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
+  callElevator(floor: number): Observable<any> {
+    return this.http.post(`${this.apiUrl}/call/${floor}`, {});
+  }
+
+  openDoors(): Observable<any> {
+    return this.http.post(`${this.apiUrl}/doors/open`, {});
+  }
+
+  closeDoors(): Observable<any> {
+    return this.http.post(`${this.apiUrl}/doors/close`, {});
+  }
+
+  startElevator(): Observable<any> {
+    return this.http.post(`${this.apiUrl}/start`, {});
+  }
+
+  stopElevator(): Observable<any> {
+    return this.http.post(`${this.apiUrl}/stop`, {});
+  }
+}
+
